a5746579a7da4bddb765c07dfbfc114eba4b5ac2
Claret Nnamocha <devclareo@gmail.com>
2020-08-28 19:39:26 +0100

    init

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Claret Nnamocha</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="mobile-menu-open" class="shadow-large">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#experience">Experience</a>
+            </li>
+            <li>
+                <a href="#education">Education</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#skills">Skills</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+    <!-- End header -->
+
+    <div id="lead">
+        <div id="lead-content">
+            <h1>Claret Nnamocha</h1>
+            <h2>Software Engineer</h2>
+            <a href="./Resume.docx" class="btn-rounded-white">Download Resume</a>
+        </div>
+        <!-- End #lead-content -->
+
+        <div id="lead-overlay"></div>
+
+        <div id="lead-down">
+            <span>
+                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            </span>
+        </div>
+        <!-- End #lead-down -->
+    </div>
+    <!-- End #lead -->
+
+    <div id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="heading">About Me</h2>
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Currently a college undergraduate with a Bachelor of Science (B.Sc.) in Computer Science with 3
+                        years of work experience. Aiming to leverage academic experience and a proven knowledge of
+                        product design, scale-up, and environmental testing. Frequently praised as results-oriented by
+                        my peers. I can be relied upon to help your organisation achieve its goals.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End #about -->
+
+    <div id="experience" class="background-alt">
+        <h2 class="heading">Experience</h2>
+        <div id="experience-timeline">
+            <div data-date="September 2018 – Date">
+                <h3>JENSIV LTD</h3>
+                <h4>Backend Developer</h4>
+                <p>
+                    JENSIV is a software company based in Owerri, Nigeria. <br>
+                    My role in this or this organisation deals with advising customer about or perform and maintenance
+                    of software system, analyzing information to determine, recommend and plan installation of a new
+                    system or modification of an existing system, Renew domain name registrations, Back up files from
+                    web sites to local directories for instant recovery in case of problems as well as direct software
+                    programming, deployment and development of documentation. <br>
+                    Website <a href="https://jensiv.com">jensiv.com</a>
+                </p>
+            </div>
+
+            <div data-date="July 2018 – September 2018">
+                <h3>Aptech Education Institute</h3>
+                <h4>Python Tutor</h4>
+            </div>
+        </div>
+    </div>
+    <!-- End #experience -->
+
+    <div id="education">
+        <h2 class="heading">Education</h2>
+        <div class="education-block">
+            <h3>Imo State University Owerri</h3>
+            <span class="education-date">Oct 2015 - Sept 2020</span>
+            <h4>Bachelor of Science in Computer Science</h4>
+            <ul>
+                <li>
+                    Relevant Coursework: Data Structures and Algorithms, Web Technologies, Artificial Intelligence and
+                    Expert Systems, Introduction to Programming (Java, C++), Database Designs, Structured Programming,
+                    Data analysis.
+                </li>
+                <li>
+                    Extracurricular Activities: Developers Students Club by Google, Developer's Circles by Facebook
+                </li>
+            </ul>
+        </div>
+        <!-- End .education-block -->
+    </div>
+    <!-- End #education -->
+
+    <div id="projects" class="background-alt">
+        <h2 class="heading">Projects</h2>
+        <div class="container">
+            <div class="row">
+                <div class="project shadow-large">
+                    <div class="project-image">
+                        <img src="images/alpha-orm.jpg" />
+                    </div>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>AlphaORM</h3>
+                        <p>
+                            AlphaORM is an SQL database library written in Node, PHP and Python. Its major aim is to
+                            make these database mimic the nosql dynamic behaviour
+                        </p>
+                        <a href="https://github.com/alph-aorm">View Project</a>
+                    </div>
+                    <!-- End .project-info -->
+                </div>
+                <!-- End .project -->
+
+                <div class="project shadow-large">
+                    <div class="project-image">
+                        <img src="images/pytonik.png" />
+                    </div>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>Pytonik</h3>
+                        <p>
+                            Pytonik is a python framework built to enhance web development, also helps web developers to
+                            build more apps with less codes. It uses expressive architectural patterns, structured on
+                            Model View Controller (MVC) and bundles of component to reuse while deploying the framework
+                        </p>
+                        <a href="https://github.com/pytonik/pytonik">View Project</a>
+                    </div>
+                    <!-- End .project-info -->
+                </div>
+                <!-- End .project -->
+            </div>
+        </div>
+    </div>
+    <!-- End #projects -->
+
+    <div id="skills">
+        <h2 class="heading">Skills</h2>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>Node.js</li>
+            <li>React Native</li>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C++</li>
+            <li>PHP</li>
+            <li>Laravel</li>
+            <li>Express</li>
+            <li>Electron</li>
+        </ul>
+    </div>
+    <!-- End #skills -->
+
+    <div id="contact">
+        <!-- <h2>Get in Touch</h2>
+        <div id="contact-form">
+            <form method="POST" action="https://formspree.io/email@email.com">
+                <input type="hidden" name="_subject" value="Contact request from personal website" />
+                <input type="email" name="_replyto" placeholder="Your email" required>
+                <textarea name="message" placeholder="Your message" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div> -->
+        <!-- End #contact-form -->
+    </div>
+    <!-- End #contact -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5 copyright">
+                    <p>
+                        Copyright &copy; 2017 Claret Nnamocha
+                    </p>
+                </div>
+                <div class="col-sm-2 top">
+                    <span id="to-top">
+                        <i class="fa fa-chevron-up" aria-hidden="true"></i>
+                    </span>
+                </div>
+                <div class="col-sm-5 social">
+                    <ul>
+                        <li>
+                            <a href="https://github.com/claretnnamocha" target="_blank"><i class="fa fa-github"
+                                    aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://stackoverflow.com/users/10803004/claret-nnamocha" target="_blank"><i
+                                    class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://linkedin.com/in/claretnnamocha" target="_blank"><i class="fa fa-linkedin"
+                                    aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/claretnnamocha" target="_blank"><i class="fa fa-facebook"
+                                    aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/claretnnamocha" target="_blank"><i class="fa fa-twitter"
+                                    aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/scripts.min.js"></script>
+</body>
+<script>
+    var bgImageArray = ['', 1, 2, 3, 4],
+        base = "./images/lead-bg",
+        secs = 4;
+    bgImageArray.forEach(function (img) {
+        new Image().src = base + img + '.jpg';
+        // caches images, avoiding white flash between background replacements
+    });
+
+    function backgroundSequence() {
+        window.clearTimeout();
+        var k = 0;
+        for (i = 0; i <= 4; i++) {
+            setTimeout(function () {
+                document.querySelector('#lead').style.background = "url(" + base + bgImageArray[k] + ".jpg) no-repeat center center fixed";
+                document.querySelector('#lead').style.backgroundSize = "cover";
+                if ((k + 1) === bgImageArray.length) { setTimeout(function () { backgroundSequence() }, (secs * 1000)) } else { k++; }
+            }, (secs * 1000) * i)
+        }
+    }
+    backgroundSequence();
+</script>
+
+</html>
